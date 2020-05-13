ac49cc8a70e763e806265ec9d22a994b31fdd913
John Simmons <johnsimmons@johns-mbp.lan>
2020-05-13 17:41:20 -0400

    Deploy

---
+++ precache-manifest.1b778004854200e040df002d81f9571a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7fbacb42004c4e9f490abb5cdf2ec9a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f85b3f80fd2469fda02e",
+    "url": "/portfolio/static/css/2.3d60f3ae.chunk.css"
+  },
+  {
+    "revision": "99442f3a781a96da1cb2",
+    "url": "/portfolio/static/css/main.0e981099.chunk.css"
+  },
+  {
+    "revision": "f85b3f80fd2469fda02e",
+    "url": "/portfolio/static/js/2.e4ba59bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.e4ba59bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99442f3a781a96da1cb2",
+    "url": "/portfolio/static/js/main.67ec394b.chunk.js"
+  },
+  {
+    "revision": "e5284505c87d0b24b753",
+    "url": "/portfolio/static/js/runtime-main.f4f2abbf.js"
+  },
+  {
+    "revision": "fbaf68fafdff888990c48d11ade85ff0",
+    "url": "/portfolio/static/media/JohnSimmonsResume.fbaf68fa.pdf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "de29fdbd505917d372d769df5d9911a8",
+    "url": "/portfolio/static/media/logo.de29fdbd.png"
+  }
+]);
